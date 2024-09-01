b9fffdfaaa5fc7f7808567b68c04431db5201eae
jihochoi <68531659+ziiho08@users.noreply.github.com>
2024-09-01 23:52:51 +0900

    Add files via upload

---
+++ edge_nerf/datamanager.py
@@ -0,0 +1,58 @@
+"""
+Depth DataManager
+"""
+
+from dataclasses import dataclass, field
+from typing import Dict, Literal, Tuple, Type, Union
+
+import torch
+
+from nerfstudio.cameras.rays import RayBundle
+from nerfstudio.data.datamanagers.base_datamanager import (
+    VanillaDataManager,
+    VanillaDataManagerConfig,
+)
+
+
+@dataclass
+class DepthDataManagerConfig(VanillaDataManagerConfig):
+    """Depth DataManager Config
+
+    Add your custom datamanager config parameters here.
+    """
+
+    _target: Type = field(default_factory=lambda: DepthDataManager)
+
+
+class DepthDataManager(VanillaDataManager):
+    """Depth DataManager
+
+    Args:
+        config: the DataManagerConfig used to instantiate class
+    """
+
+    config: DepthDataManagerConfig
+
+    def __init__(
+        self,
+        config: DepthDataManagerConfig,
+        device: Union[torch.device, str] = "cpu",
+        test_mode: Literal["test", "val", "inference"] = "test", # 240826
+        world_size: int = 1,
+        local_rank: int = 0,
+        **kwargs,  # pylint: disable=unused-argument
+    ):
+        super().__init__(
+            config=config, device=device, test_mode=test_mode, world_size=world_size, local_rank=local_rank, **kwargs
+        )
+
+    def next_train(self, step: int) -> Tuple[RayBundle, Dict]:
+        """Returns the next batch of data from the train dataloader."""
+        self.train_count += 1
+        image_batch = next(self.iter_train_image_dataloader)
+        assert self.train_pixel_sampler is not None
+        assert isinstance(image_batch, dict)
+        batch = self.train_pixel_sampler.sample(image_batch)
+        ray_indices = batch["indices"]
+        ray_bundle = self.train_ray_generator(ray_indices)
+        return ray_bundle, batch
